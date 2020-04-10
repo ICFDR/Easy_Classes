5d82219d35ea0a2629f9344e7fab572b07063d24
Sarthak Vineet Kumar <sarthakchaudhary13@gmail.com>
2020-04-10 22:21:25 +0530

    gitignore

--- website/models.py
+++ website/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+ class Slider(models.Model):
+
+     heading = models.CharField()
+
+     description = models.CharField()
+
+     image = models.ImageField()
+
+
+ class Vision(models.Model):
+
+     pass
+
+
+ class Events(models.Model):
+
+     pass
+
+
+ class
