0f6576dc20a07990cd3266831c60baaa15f41427
pagalprogrammer <pagalprogrammer@protonmail.com>
2020-04-10 14:27:13 +0530

    Heroku Setup: Required Files Added

--- Eassy_Classes/settings.py
+++ Eassy_Classes/settings.py
@@ -120,4 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
