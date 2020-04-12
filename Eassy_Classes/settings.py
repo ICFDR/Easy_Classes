5cc9281bc5358e876b4a17ccae6a6c637aaf00cb
Nagendra Kumar <40954241+Nagendracse1@users.noreply.github.com>
2020-04-12 16:48:09 +0530

    media space removed

--- Eassy_Classes/settings.py
+++ Eassy_Classes/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_ROOT = os.path.join(BASE_DIR , "media")
-MEDIA_URL = "/media /"
+MEDIA_URL = "/media/"
 
 
 # Quick-start development settings - unsuitable for production
