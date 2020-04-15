5d2159a9d5d682c1ee829f4af466ae3b0f2de722
pagalprogrammer <pagalprogrammer@protonmail.com>
2020-04-15 10:25:12 +0530

    Carousel Modified

--- Eassy_Classes/urls.py
+++ Eassy_Classes/urls.py
@@ -18,10 +18,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-admin.site.site_header = "iCFDR Admin"
-admin.site.site_title = "iCFDR Admin Portal"
-admin.site.index_title = "Welcome to iCFDR Administration Portal"
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
--- admin_panel/admin.py
+++ admin_panel/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+admin.site.site_header = "iCFDR Admin"
+admin.site.site_title = "iCFDR Admin Portal"
+admin.site.index_title = "Welcome to iCFDR Administration Portal"
+admin.site.unregister(Group)
