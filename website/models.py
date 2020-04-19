645996e0a71cd047ae5394c172085dc2312dc5e1
pagalprogrammer <pagalprogrammer@protonmail.com>
2020-04-19 14:50:58 +0530

    Vision Section is now dynamic

--- website/models.py
+++ website/models.py
@@ -49,7 +49,7 @@ class Vision(models.Model) :
 
 
 class VisionIcons(models.Model):
-    icon = models.ImageField(upload_to = 'images/vision-icons/')
+    icon = models.CharField(max_length = 20)
 
     icon_name = models.CharField(max_length = 50)
 
