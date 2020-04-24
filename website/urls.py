02665ee7ce324af5be81c6f307d838fb056b4cd4
pagalprogrammer <pagalprogrammer@protonmail.com>
2020-04-24 15:25:31 +0530

    New Structure: Breakdown of Pages

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,7 @@ from .import views
 urlpatterns = [
     path('', views.index,name = 'home'),
     path('blogs/<int:id>',views.blog_view,name = 'blog'),
-    path('blogs',views.blog_view,name = 'allBlogs'),
+    path('blogs',views.bloglist,name = 'bloglist'),
+    path('gallery',views.gallery,name = 'gallery'),
+    path('about',views.about,name = 'about')
 ]
--- website/views.py
+++ website/views.py
@@ -15,31 +15,52 @@ def index(request,moveToBlogs=None):
         'GalleryRow1':Gallery.objects.all()[:4],
         'GalleryRow2':Gallery.objects.all()[4:8],
         'OurCauses':OurCauses.objects.all(),
-        'AboutSWLP':AboutSWLP.objects.all(),
-        'AboutSWLPIcons':AboutSWLPIcons.objects.all(),
         'JoinUs':JoinUs.objects.all(),
-        'LeaderSaysSection':LeaderSaysSection.objects.all(),
-        'LeaderSays':LeaderSays.objects.all(),
-        'BoardTeam':BoardTeam.objects.all(),
-        'OrganizingTeam':OrganizingTeam.objects.all(),
         'OurChildrensSection':OurChildrensSection.objects.all(),
         'OurChildrens':OurChildrens.objects.all(),
         'BlogSection':BlogSection.objects.all(),
-        'Blogs':Blogs.objects.all(),
-		'moveToBlogs':moveToBlogs,
+        'Blogs':Blogs.objects.all().order_by('-id')[:3]
     }
 
     return render(request, 'index.html', data)
 
-
 def blog_view(request,id=None):
     blog = Blogs.objects.filter(id = id)
-
-    blogs = Blogs.objects.all().order_by('-id')
 
     if blog:
         data = {'blog':blog[0]}
 
         return render(request, 'blog.html', data)
 
-    return index(request,'1')
+    return bloglist(request)
+
+def bloglist(request):
+
+    data = {
+        'BlogSection':BlogSection.objects.all(),
+        'Blogs':Blogs.objects.all().order_by('-id')
+    }
+
+    return render(request, 'bloglist.html', data)
+	
+def gallery(request):
+
+    data = {
+        'Gallery':Gallery.objects.all()
+    }
+
+    return render(request, 'gallery.html', data)
+	
+	
+def about(request):
+
+    data = {
+        'AboutSWLP':AboutSWLP.objects.all(),
+        'AboutSWLPIcons':AboutSWLPIcons.objects.all(),
+        'LeaderSaysSection':LeaderSaysSection.objects.all(),
+        'LeaderSays':LeaderSays.objects.all(),
+        'BoardTeam':BoardTeam.objects.all(),
+        'OrganizingTeam':OrganizingTeam.objects.all(),
+    }
+
+    return render(request, 'about.html', data)
