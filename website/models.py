4a516249e938d494fea2d964212556854333a6c3
Nagendra Kumar <nagendra.cse1@gmail.com>
2020-05-04 18:15:17 +0530

    organising media file will automatically delete when uou delete it from admin panel

--- website/models.py
+++ website/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from datetime import date
 from django.utils.text import slugify
+from django.dispatch import receiver
+import os
 
 class AboutUs(models.Model):
     heading = models.CharField(max_length=100)
@@ -161,6 +163,32 @@ class OrganizingTeam(models.Model):
 
     def __str__(self):
         return self.name
+
+
+@receiver(models.signals.post_delete,sender = OrganizingTeam )
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
+
+
+@receiver(models.signals.pre_save,sender = OrganizingTeam)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = sender.objects.get(pk=instance.pk).image
+    except sender.DoesNOtExist:
+        return False
+
+    new_file = instance.image
+
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
 
 
 class OurChildrensSection(models.Model):
