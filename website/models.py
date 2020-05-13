a153b36dad384c84f95663e6483ef4d4f350d9b9
Nagendra Kumar <nagendra.cse1@gmail.com>
2020-05-13 19:53:33 +0530

    models updated

--- website/models.py
+++ website/models.py
@@ -259,3 +259,29 @@ class Donate(models.Model):
     image = models.ImageField(upload_to='images/donate/')
 
     bank_info = RichTextField()
+
+
+@receiver(models.signals.post_delete,sender = Donate )
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
+
+
+@receiver(models.signals.pre_save,sender = Donate)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = sender.objects.get(pk=instance.pk).image
+    except sender.DoesNOtExist:
+        return False
+
+    new_file = instance.image
+
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
