d775381d366cc4948074e56a9cff6fdbcae8d854
Nagendra Kumar <nagendra.cse1@gmail.com>
2020-05-14 20:44:56 +0530

    auto session bug fixed

--- website/models.py
+++ website/models.py
@@ -271,24 +271,31 @@ class Donate(models.Model):
 
 
 @receiver(models.signals.post_delete)
-def auto_delete_file_on_delete(sender, instance, **kwargs):
-    if instance.image:
-        if os.path.isfile(instance.image.path):
-            os.remove(instance.image.path)
+def auto_delete_file_on_delete(instance, **kwargs):
+    try:
+        if instance.image:
+            if os.path.isfile(instance.image.path):
+                os.remove(instance.image.path)
+    except:
+        return False
 
 
 @receiver(models.signals.pre_save)
 def auto_delete_file_on_change(sender, instance, **kwargs):
-    if not instance.pk:
-        return False
-
     try:
-        old_file = sender.objects.get(pk=instance.pk).image
-    except sender.DoesNOtExist:
+        if not instance.pk:
+            return False
+
+        try:
+            old_file = sender.objects.get(pk=instance.pk).image
+        except sender.DoesNOtExist:
+            return False
+
+        new_file = instance.image
+
+        if not old_file == new_file:
+            if os.path.isfile(old_file.path):
+                os.remove(old_file.path)
+
+    except:
         return False
-
-    new_file = instance.image
-
-    if not old_file == new_file:
-        if os.path.isfile(old_file.path):
-            os.remove(old_file.path)
