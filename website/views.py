43f052bbc59e098f5007481872dd441416449e69
Nagendra Kumar <nagendra.cse1@gmail.com>
2020-05-27 15:16:39 +0530

    fellow

--- website/views.py
+++ website/views.py
@@ -103,6 +103,7 @@ def fellowship(request):
     data = {
         'fellowship':Fellowship.objects.all()[0],
         'images':FellowshipImages.objects.filter(fellowship=Fellowship.objects.all()[0]),
+        'fellow_says':FellowSays.objects.filter(fellowship=Fellowship.objects.all()[0]),
         'campaigns': Campaign.objects.all().order_by('-id')[:5],
     }
 
